e047ec51734d897206237636f252b82c9fe957ff
Dmitrii Repnikov <dmitrii.amagkn@gmail.com>
2023-06-11 19:57:43 +0300

    small refactor

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,7 +1,7 @@
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const commonConfig = require("./webpack.common");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 
 const devConfig = {
